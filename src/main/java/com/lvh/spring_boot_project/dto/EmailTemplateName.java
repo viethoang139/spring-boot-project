72e77bb9ab240d4573f441738cc1ee932c568130
Le Viet Hoang <levieth710@gmail.com>
2024-07-10 10:12:48 +0700

    update EmailTemplateName

--- src/main/java/com/lvh/spring_boot_project/dto/EmailTemplateName.java
+++ src/main/java/com/lvh/spring_boot_project/dto/EmailTemplateName.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum EmailTemplateName {
-    ACTIVATE_ACCOUNT("activate_account");
+    ACTIVATE_ACCOUNT("activate_account"),
+    NOTIFICATION_EMAIL("notification_email");
     private final String name;
 
     EmailTemplateName(String name){
