d8228610b52dd987eabfa6866845885515165bc4
Le Viet Hoang <levieth710@gmail.com>
2024-07-10 10:11:52 +0700

    add findAllUser method in UserRepository

--- src/main/java/com/lvh/spring_boot_project/repository/UserRepository.java
+++ src/main/java/com/lvh/spring_boot_project/repository/UserRepository.java
@@ -2,11 +2,19 @@ package com.lvh.spring_boot_project.repository;
 
 import com.lvh.spring_boot_project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByEmail(String email);
+
+    @Query("""
+         select u from User u
+         where u.enabled = true 
+        """)
+    List<User> findAllUser();
 
 }
